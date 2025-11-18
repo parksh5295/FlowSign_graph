490ddf9b756ee944f8bb81bcdd8340ebd727ad5a
parksh5295 <parksh5295@gmail.com>
2025-11-18 14:36:05 +0900

    범례

--- plot1.py
+++ plot1.py
@@ -88,7 +88,7 @@ def main():
                ncol=2, 
                frameon=True,
                fontsize=82,
-               bbox_to_anchor=(0.5, 1.04),
+               bbox_to_anchor=(0.5, 1.03),
                bbox_transform=fig.transFigure)
     
     fig.tight_layout(rect=[0, 0.09, 1, 0.90])
