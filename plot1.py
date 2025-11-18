0ce61a4196539738d56f27f57b963dc68e0b357f
parksh5295 <parksh5295@gmail.com>
2025-11-18 14:20:18 +0900

    범례

--- plot1.py
+++ plot1.py
@@ -88,10 +88,10 @@ def main():
                ncol=2, 
                frameon=True,
                fontsize=82,
-               bbox_to_anchor=(0.5, 1.06),
+               bbox_to_anchor=(0.5, 1.02),
                bbox_transform=fig.transFigure)
     
-    fig.tight_layout(rect=[0, 0.09, 1, 0.92])
+    fig.tight_layout(rect=[0, 0.09, 1, 0.95])
 
     # Output folder: ../Graph
     graph_dir = base_dir.parent / "Graph"
